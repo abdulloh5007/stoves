f366dac44457a4ecd77284d3669a740fa7440a62
Abdulloh <abdulloh50007@gmail.com>
2025-07-26 17:19:03 +0000

    V 1.1 release

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Qozonlar savdosi',
-  description: 'Qozon sotish uchun ilova',
+  title: 'Katyollar savdosi',
+  description: 'Katyol sotish uchun ilova',
 };
 
 export default function RootLayout({
